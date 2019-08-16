91f985814c05162c4602ac0edd4960ed7f2f9a79
Tobi Balogun <tobilexog@gmail.com>
2019-08-16 04:40:38 +0200

    adding basic login form to test react-redux flow

---
+++ client/src/components/app.js
@@ -0,0 +1,17 @@
+// React
+import React, { Component, PropTypes } from 'react';
+
+// Components
+import Signin from './auth/signin_component.js';
+
+const App = () => {
+    
+    return (
+        <div>
+            Test test test
+            { this.props.children }
+        </div>
+    );    
+};
+
+export default App;
